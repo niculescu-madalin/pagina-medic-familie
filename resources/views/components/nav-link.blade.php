6b53152b3b015e4ac651674e49a47764a8798610
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-04-04 17:36:49 +0300

    Changed navbar style

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,9 +1,25 @@
 @props(['active'])
 
 @php
+$activeClasses = 'inline-flex items-center '.
+    'px-6 pt-1 my-2 ' . 
+    'rounded-lg bg-white shadow '.
+    'text-md font-semibold '.
+    'focus:outline-none focus:bg-gray-100 '.
+    'transition duration-150 ease-in-out ';
+
+$inactiveClasses = 'inline-flex items-center '.
+    'px-6 pt-1 my-2 '.
+    'rounded-lg '.
+    'text-md font-medium text-white '.
+    'hover:text-gray-700 hover:border-gray-300' .
+    'hover:bg-white '.
+    'focus:outline-none focus:text-gray-100 focus:border-gray-300 '.
+    'transition duration-150 ease-in-out';
+
 $classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            ? $activeClasses
+            : $inactiveClasses;
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav x-data="{ open: false }" class="bg-cyan-800 border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-teal-600 border-b border-sky-900">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <x-application-logo class="block h-9 w-auto fill-current text-white" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-1 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Despre') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('contact')">
+                        {{ __('Contact') }}
                     </x-nav-link>
                 </div>
             </div>
