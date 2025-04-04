fb983e3066779e5186b5a5498a26a76e1756d84b
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-04-04 16:48:21 +0300

    Added Tailwind

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
         <title>{{ config('app.name', 'Cabinet Medical') }}</title>
 
