c8e39736a4964c89315dc67b45436c10ed43b5cc
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-04-05 20:58:10 +0300

    Added contatc route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
