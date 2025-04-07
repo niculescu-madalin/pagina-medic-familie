909afb7484759a21c1b4527f91074038820dc90a
niculescu-madalin <116899982+niculescu-madalin@users.noreply.github.com>
2025-04-07 22:18:22 +0300

    Added privacy policy page

---
+++ resources/views/legal/privacy.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+    <div class="mx-auto md:max-w-4xl pt-32 py-20 px-10 grid grid-cols-1">
+        <h1 class="pb-2 font-semibold text-2xl"> Politica de confidentialitate</h1>
+        <p>
+            Considerăm asigurarea dreptului la protecția datelor cu caracter personal ca un angajament fundamental, prin urmare vom dedica toate resursele și eforturile necesare pentru a prelucra datele dumneavoastră în deplină concordanță cu Regulamentul (UE) 2016/679 (“Regulamentul general privind protecția datelor” sau “GDPR”), precum și cu orice altă legislație aplicabilă pe teritoriul Romaniei. Întrucât unul dintre principiile esențiale ale acestui cadrul legal îl reprezintă transparența, am pregătit aceast document prin care dorim să vă informăm despre modul în care colectăm, utilizăm, transferăm și protejăm datele dvs cu caracter personal atunci când interacționați cu noi în legătură cu produsele și serviciile noastre, inclusiv prin site-ul nostru web.
+            Ne rezervăm dreptul de a actualiza și modifica periodic această Politică de Confidențialitate, pentru a reflecta orice modificări ale modului în care prelucrăm datele dumneavoastră cu caracter personal sau orice modificări ale cerințelor legale. În cazul oricărei astfel de modificări, vom afișa pe website-ul nostru versiunea modificată a Politicii de Confidențialitate, motiv pentru care va rugam sa verificati periodic continutul acestei Politici de Confidentialitate.
+        </p>
+
+        <h4 class="pt-4 pb-2 font-semibold text-xl">Ce sunt datele cu caracter personal? </h4>
+
+“Date cu caracter personal” înseamnă orice informaţii privind o persoană fizică identificată sau identificabilă (“persoana vizată”) în special prin referire la un element de identificare, cum ar fi un nume, un număr de identificare, date de localizare, un identificator online, sau la unul sau mai multe elemente specifice, proprii identităţii sale fizice, fiziologice, genetice, psihice, economice, culturale sau sociale.
+
+<h4 class="pt-4 pb-2 font-semibold text-xl">Ce înseamnă prelucrarea datelor cu caracter personal?</h4>
+
+“Prelucrare” înseamnă orice operaţiune sau set de operaţiuni efectuate asupra datelor cu caracter personal sau asupra seturilor de date cu caracter personal, cu sau fără utilizarea de mijloace automatizate, cum ar fi colectarea, înregistrarea, organizarea, structurarea, stocarea, adaptarea sau modificarea, extragerea, consultarea, utilizarea, divulgarea prin transmitere, diseminarea sau punerea la dispoziţie în orice alt mod, alinierea sau combinarea, restricţionarea, ştergerea sau distrugerea.
+
+<h4 class="pt-4 pb-2 font-semibold text-xl">Ce categorii de date cu caracter personal prelucrăm</h4>
+
+Prelucram datele angajatilor (actuali, fosti si potentiali), ai partenerilor si ai clientilor nostri. În general, colectăm datele dvs. cu caracter personal direct de la dumneavoastră (atat in cadrul platformelor online cat si in cadrul clinicii medicale), astfel încât aveți controlul asupra tipului de informație pe care ne-o oferiți.
+
+Cu titlul de exemplu, primim informații de la dvs. astfel:
+<ul class="list-disc list-inside space-y-2 text-base py-2">
+    <li>Atunci când apelați la serviciile noastre medicale și completați fișa medicală, ne transmiteți: adresa de e-mail, numele și prenumele, codul numeric personal, numărul de telefon etc.</li>
+    <li>Prin platformele online sau recepție, pentru programarea la un consult medical, ne transmiteți: numele, adresa de email și numărul de telefon.</li>
+    <li>Prin platformele online, ne transmiteți datele de navigare pe website-uri (adresa IP, tipul browser-ului folosit, paginile vizitate).</li>
+    <li>Prin conturile făcute în secțiunea forum de discuții online, ne transmiteți numele, adresa de email și comentariile dumneavoastră.</li>
+    <li>În cadrul serviciilor medicale oferite, ne transmiteți date medicale sensibile.</li>
+</ul>
+
+    </div>
+</x-app-layout>
